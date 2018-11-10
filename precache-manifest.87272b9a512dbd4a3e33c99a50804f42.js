ef74829de6d0b9b4101d54e3a7213a768ec3fd97
Balasubramani M <bala.m@live.in>
2018-11-10 10:04:51 +0530

    Updates

---
+++ precache-manifest.87272b9a512dbd4a3e33c99a50804f42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "aeab848c26357f3d120f3e58f1efa8f5",
+    "url": "/wakecap/static/media/search.aeab848c.svg"
+  },
+  {
+    "revision": "ac24e064ad04837f8c07",
+    "url": "/wakecap/static/js/runtime~main.ac24e064.js"
+  },
+  {
+    "revision": "37acfdec133d5ca200d0",
+    "url": "/wakecap/static/js/main.37acfdec.chunk.js"
+  },
+  {
+    "revision": "e20ee7960abe6de81f6e",
+    "url": "/wakecap/static/js/1.e20ee796.chunk.js"
+  },
+  {
+    "revision": "37acfdec133d5ca200d0",
+    "url": "/wakecap/static/css/main.d73e4501.chunk.css"
+  },
+  {
+    "revision": "e20ee7960abe6de81f6e",
+    "url": "/wakecap/static/css/1.96679f27.chunk.css"
+  },
+  {
+    "revision": "fa4d1cea83ad0490702cf0962748dc6e",
+    "url": "/wakecap/index.html"
+  }
+];
